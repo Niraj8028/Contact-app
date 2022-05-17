012c9302cc444a489b87de5839bb0f218d3e0717
Niraj Pardeshi <nirajpardeshi2002@gmail.com>
2022-05-17 20:00:12 +0530

    Layout completed

--- src/layout/Header.js
+++ src/layout/Header.js
@@ -7,10 +7,11 @@ import { Link } from "react-router-dom";
 const Header = () => {
   return (
     <Navbar color="info" light>
-      <NavbarBrand className="text-white">LCO Contact App</NavbarBrand>
+      <NavbarBrand className="text-white" ><Link to="/">My Contact App</Link></NavbarBrand>
       <NavbarText className="text-white float-right">
         A simple Contact app
       </NavbarText>
     </Navbar>
   );
 };
+export default Header;
